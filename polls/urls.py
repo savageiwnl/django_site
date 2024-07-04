90c9848cf85f31d01363633caffd0226b44d62ac
savageiwnl <savagegamesgod@gmail.com>
2024-07-04 15:25:16 +0300

    static

--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,7 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
+from django.contrib import admin
 
 from . import views
 
@@ -9,3 +12,6 @@ urlpatterns = [
     path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
